8dddf283abc066c9078d80e0bac26ec45b1946b1
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-09-19 19:11:35 +0545

    initial_commit
    First phase development

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import JSONDATA from "./MOCK_DATA.json"
+import "./App.css"
+import { useState } from "react"
+const App =()=>{
+  const [searchTerm , setSearchTerm]= useState();
+  
+  return (
+    <>
+    <div className="App" >
+      <input  className="input" type="text" placeholder="Search...."
+      onChange={e=>{setSearchTerm(e.target.value)}}/>
+      {
+        JSONDATA.
+        filter((val =>{
+          if(searchTerm==="" ){
+            return val
+          } else if (val.first_name.toLowerCase().toLowerCase().includes(searchTerm)){
+            return val
+          }
+        }))
+        .map((val, key)=>{
+          return (<>
+          <div className="user" key={key}>
+          <p >   {val.first_name}</p>
+          </div>
+          </>)
+        })
+      }
+    </div> 
+    </>
+  )
+
+}
+export default App;
